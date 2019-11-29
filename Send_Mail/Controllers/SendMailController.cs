d2dbaa3be46eb1ac202645df94d84805f05b6454
张韬 <18872046079@163.com>
2019-11-29 09:01:28 +0800

    first commit

---
+++ Send_Mail/Controllers/SendMailController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Send_Mail.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SendMailController : Controller
+    {
+        public SendMailController()
+        {
+           
+        }
+
+        /// <summary>
+        /// 发送邮件
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<String> Send()
+        {
+            return "";
+        }
+    }
+}
---
+++ Send_Mail/Startup.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.PlatformAbstractions;
+using Microsoft.OpenApi.Models;
+
+namespace Send_Mail
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+                #region
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                //Send_Mail.xml是我的项目生成XML文档的后缀名,具体的以你项目为主
+                var xmlPath = Path.Combine(basePath, "Send_Mail.xml");
+                c.IncludeXmlComments(xmlPath);
+                #endregion
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            //启用中间件服务生成Swagger作为JSON终结点
+            app.UseSwagger();
+            //启用中间件服务对swagger-ui，指定Swagger JSON终结点
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+            app.UseHttpsRedirection(); 
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
